88f2c43ca3e3d47e15aacd5c58f47ff04f20ef26
Alisa Chernysheva <chernysheva.alisa@gmail.com>
2021-05-02 21:06:30 +0200

    initial commit

---
+++ src/main/java/fr/formation/developers/controllers/SkillController.java
@@ -0,0 +1,23 @@
+package fr.formation.developers.controllers;
+
+import fr.formation.developers.domain.Skill;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+public class SkillController {
+    @GetMapping("/skills/{id}")
+    public Skill getById(@PathVariable("id") Long id) {
+        Skill skill = new Skill();
+        skill.setName("Spring boot " + id);
+        return skill;
+    }
+
+    @PostMapping("/skills")
+    public void create(@RequestBody Skill skill) {
+        System.out.println(skill);
+    }
+}
---
+++ src/main/java/fr/formation/developers/domain/Skill.java
@@ -0,0 +1,21 @@
+package fr.formation.developers.domain;
+
+public class Skill {
+    private String name;
+
+    public Skill() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Skill [name=" + name + "]";
+    }
+}
